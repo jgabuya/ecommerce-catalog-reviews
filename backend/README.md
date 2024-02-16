c9f9f8ccc23d9bbe51b596f7244fbf5566add9d5
jgabuya <boyhimas@gmail.com>
2024-02-16 20:13:31 +0100

    init prisma, add product and category models

---
+++ backend/README.md
@@ -0,0 +1,13 @@
+## Prisma
+
+### migration
+
+```sh
+npx prisma migrate dev --name descriptive_migration_name
+```
+
+### generate client
+
+```sh
+npx prisma generate
+```
---
+++ backend/prisma/migrations/20240216191027_init_add_product_category/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "ProductCategory" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "ProductCategory" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
