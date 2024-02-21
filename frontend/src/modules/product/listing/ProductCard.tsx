9954e4451bfd198e34fc58a8894a9f9fd1731a6b
jgabuya <boyhimas@gmail.com>
2024-02-21 16:06:31 +0100

    fix card grid alignment

--- frontend/src/modules/product/listing/ProductCard.tsx
+++ frontend/src/modules/product/listing/ProductCard.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const ProductCard: React.FC = () => (
-  <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+  <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto">
     <a href="#">
       <img
-        className="p-8 rounded-t-lg"
         src="https://placehold.co/899x567?text=Product image"
         alt="product image"
+        width={899}
+        height={567}
+        className="p-8 rounded-t-lg"
       />
     </a>
     <div className="px-5 pb-5">
       <a href="#">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          Product name
         </h5>
       </a>
       <div className="flex items-center mt-2.5 mb-5">
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Nav />
       <main className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-center justify-center mx-auto">
           <ProductCard />
           <ProductCard />
           <ProductCard />
